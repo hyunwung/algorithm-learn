90e632ec16f896090ce7a731950c773372ef0234
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-10-10 15:33:38 +0900

    문제 풀기

---
+++ 구명보트.py
@@ -0,0 +1,9 @@
+def solution(people, limit):
+    answer = 0
+    half = limit//2
+    print(half)
+    for i in range(len(people)):
+        print(i,"g")
+    return answer
+    
+solution([70, 50, 80, 50],100)
