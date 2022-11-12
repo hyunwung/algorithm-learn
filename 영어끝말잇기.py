00e9742e007f5c994ff2069672bd5f49f776b88d
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-11-12 12:49:12 +0900

    Update

--- 영어끝말잇기.py
+++ 영어끝말잇기.py
@@ -1,3 +1,27 @@
+from collections import Counter
+
 def solution(n, words):
     answer = []
-    return answer
+    check=False
+    for i in range(len(words)):
+        answer.append(words[i])
+        if len(answer) != len(set(answer)):
+            print("중복")
+            check = True
+        if i == len(words)-1:
+            return [0,0]
+
+        if words[i][-1] != words[i+1][:1] or check == True:
+            num1 = (i+1)//n
+            if (i+2)%n == 0:
+
+                return [n,num1]
+            else:
+
+                return [(i+2)%n,num1+1]
+
+
+
+
+
+solution(3,	["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])
