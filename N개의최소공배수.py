f630ecee0461a1ea0fa440351effc237e9a274f6
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-11-15 23:47:15 +0900

    Update

---
+++ N개의최소공배수.py
@@ -0,0 +1,31 @@
+from math import lcm
+from functools import reduce
+
+arr = [2,6,8,14]
+#
+def solution(arr):
+    arr.sort()
+    n = 1
+
+    while True:
+        answer = max(arr) * n  # 가장 큰 수의 배수 기준으로 최소공배수를 구함.
+        result = True  # if result=True: 최소공배수    else: 최소공배수가 아님
+        for num in arr:
+            if answer % num != 0:
+                result = False  # answer가 나누어 떨어지지 않으면 result=False로 변경 후 break
+                break
+        if result:  # result 판별 True이면 while True문을 빠져나옴
+            break
+        n += 1
+        print(n)
+
+    return answer
+
+
+# print(solution(arr))
+
+#
+# def solution2(arr):
+#     return reduce(lambda x,y:lcm(x,y),arr)
+#
+# print(solution2(arr))
