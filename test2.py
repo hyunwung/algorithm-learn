d5448ff33d710c52b61696f28ef16be8c7b33a38
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-12-07 14:59:22 +0900

    update

--- test2.py
+++
@@ -1,6 +0,0 @@
-def solution(a,b):
-    answer = a+b
-    print(answer)
-    return answer
-
-solution(1,2)
--- 타겟넘버.py
+++ 타겟넘버.py
@@ -1,33 +1,16 @@
-# def solution(numbers, target):
-#     answer = 0
-#     for i in numbers:
-#         for j in range(len(numbers)):
-#             if sum(numbers) == target:
-#                 answer = answer+1
-#             else:
-#                 numbers[j] =numbers[j] * -1
-#             print(sum(numbers))
-#         nonlocal
-#     return answer
-#
-# solution([4, 1, 2, 1],4)
-
-answer = 0
-def DFS(idx, numbers, target, value):
-    global answer
-    N = len(numbers)
-    if(idx== N and target == value):
-        answer += 1
-        return
-    if(idx == N):
-        return
-
-    DFS(idx+1,numbers,target,value+numbers[idx])
-    DFS(idx+1,numbers,target,value-numbers[idx])
-
 def solution(numbers, target):
-    global answer
-    DFS(0,numbers,target,0)
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    print(answer)
     return answer
-
 solution([4, 1, 2, 1],4)
