3270aee6a439326aeb1540771795e5af784cdf08
hyunwungSEO <sddsk1123@naver.com>
2022-12-21 15:35:26 +0900

    Update

--- 정수내림차순으로배치하기.py
+++ 정수내림차순으로배치하기.py
@@ -1,3 +1,11 @@
 def solution(n):
-    answer = 0
-    return answer
+    answer = []
+    answer2 = ""
+    for i in str(n):
+        answer.append(int(i))
+    answer.sort()
+    for i in answer:
+        answer2 = str(i)+answer2
+    return int(answer2)
+
+solution(118372)
