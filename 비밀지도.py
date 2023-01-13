2953d0847199aa9d7058e92bc6f0e2bcdd871ae9
hyunwungSEO <sddsk1123@naver.com>
2023-01-13 16:22:19 +0900

    update

--- 비밀지도.py
+++ 비밀지도.py
@@ -1,5 +1,5 @@
 def solution(n, arr1, arr2):
-    answer = []
+    result = []
     a = []
     b = []
     for i in arr1:
@@ -15,13 +15,17 @@ def solution(n, arr1, arr2):
         else:
             b.append(bin(i)[2:])
     for i in range(len(a)):
-        for j in a[i]:
-            print(j)
-            break
-
-    print(a)
-    print(b)
-    return answer
+        d = ""
+        for j in range(len(a[i])):
+            if a[i][j] != b[i][j]:
+                d = d + "#"
+            else:
+                if a[i][j] == "0":
+                    d = d + " "
+                if a[i][j] == "1":
+                    d = d + "#"
+        result.append(d)
+    return result
 
 solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28])
 
